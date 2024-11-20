80af676f45613913392d46cd033ea975718211b3
hwanghyeonseo <hhseo1202@naver.com>
2024-11-20 11:30:04 +0900

    :sparkles:fix: domain class update

---
+++ src/main/java/com/memorytree/forest/domain/Diary.java
@@ -0,0 +1,33 @@
+package com.memorytree.forest.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Table(name = "diary")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Diary {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "diary_when")
+    private Date when;
+
+    @Column(name = "diary_what")
+    private String what;
+
+    @Column(name = "diary_who")
+    private String who;
+}
+
