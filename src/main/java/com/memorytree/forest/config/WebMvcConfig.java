4ffb4596bb3338e9492d49f97471f594a562dc53
dorkem <chlwogur1000@gmail.com>
2024-11-22 18:49:33 +0900

    :recycle: refactor: 불필요한 코드 제거

--- src/main/java/com/memorytree/forest/config/WebMvcConfig.java
+++ src/main/java/com/memorytree/forest/config/WebMvcConfig.java
@@ -1,23 +1,8 @@
 package com.memorytree.forest.config;
 
-import com.memorytree.forest.exception.UserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final UserIdArgumentResolver userIdArgumentResolver;
-
-    public WebMvcConfig(UserIdArgumentResolver userIdArgumentResolver) {
-        this.userIdArgumentResolver = userIdArgumentResolver;
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(userIdArgumentResolver);
-    }
 }
