f5ce9e53ac19b7769be5b5ff9fde17f769983575
10cheon00 <10cheon00@gmail.com>
2024-11-24 02:44:26 +0900

    :bug: fix: use custom controller instead of oauth2client

--- src/main/java/com/memorytree/forest/controller/KakaoOAuthController.java
+++ src/main/java/com/memorytree/forest/controller/KakaoOAuthController.java
@@ -3,6 +3,7 @@ package com.memorytree.forest.controller;
 import com.memorytree.forest.dto.global.ResponseDto;
 import com.memorytree.forest.dto.response.KakaoUserInfoResponseDto;
 import com.memorytree.forest.service.KakaoOAuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/oauth2")
+@RequestMapping("/api/auth/login/code")
 @RequiredArgsConstructor
 public class KakaoOAuthController {
 
     private final KakaoOAuthService kakaoOAuthService;
 
-    @GetMapping("/callback/kakao")
-    public ResponseEntity<ResponseDto<KakaoUserInfoResponseDto>> handleKakaoCallback(@RequestParam String code) {
+    @GetMapping("/kakao")
+    public ResponseEntity<ResponseDto<KakaoUserInfoResponseDto>> handleKakaoCallback(@RequestParam String code, HttpServletRequest request) {
         KakaoUserInfoResponseDto userInfo = kakaoOAuthService.getKakaoUserInfo(code);
         return ResponseEntity.ok(ResponseDto.ok(userInfo));
     }
