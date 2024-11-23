44512a61e3b011fef420f3f3375c925591fd8c14
10cheon00 <10cheon00@gmail.com>
2024-11-24 00:25:43 +0900

    :bug: fix: allow origin all, method all, headers all for CORS

--- src/main/java/com/memorytree/forest/config/SecurityConfig.java
+++ src/main/java/com/memorytree/forest/config/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 import java.util.Map;
 
 @Configuration
@@ -32,7 +35,13 @@ public class SecurityConfig {
                         .successHandler(oAuth2AuthenticationSuccessHandler())
                 )
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable());
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("*"));
+                    configuration.setAllowedMethods(Arrays.asList("*"));
+                    configuration.setAllowedHeaders(Arrays.asList("*"));
+                    return configuration;
+                }));
 
         return http.build();
     }
