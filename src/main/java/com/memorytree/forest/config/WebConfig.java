53100b0aa1fd344a7b4e6045ae5a99b519310753
hwanghyeonseo <hhseo1202@naver.com>
2024-11-24 01:33:58 +0900

    :bug:fix: cors

--- src/main/java/com/memorytree/forest/config/WebConfig.java
+++ src/main/java/com/memorytree/forest/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.memorytree.forest.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+
 }
 
